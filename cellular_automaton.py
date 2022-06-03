c348876353051b11b544ed666e8ca3f2bc6297fb
Roman <romanmutel@gmail.com>
2022-06-03 17:40:32 +0300

    added basic classes

---
+++ cellular_automaton.py
@@ -0,0 +1,7 @@
+
+class CellularAutomaton:
+    '''
+    Class that represents a grid of people, who interact together and form a society.
+    '''
+    def __init__(self, width, height, ua_percentage, youth_percentage, fill):
+        pass
--- person.py
+++ person.py
@@ -1,12 +1,21 @@
 
 class Person:
+    '''
+    Class that describes a single person
+
+    Possible states:
+        -active_ua
+        -passive_ua
+        -surzhyk
+        -passive_ru
+        -active_ru
+    '''
     def __init__(self, age, region, coordinates, ukrlang_parents):
         self.age = age
         self.region = region
         self.coordinates = coordinates
         self.parents = ukrlang_parents
-        self.lang = None
+        self.state = None
 
     def __str__(self) -> str:
-        return f'Person({self.age}, {self.region}, {self.coordinates}, {self.parents}) is {self.lang} speaker'
-    
+        return f'Person({self.age}, {self.region}, {self.coordinates}, {self.parents}) is in {self.state} state'
