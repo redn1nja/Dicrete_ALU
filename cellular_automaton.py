73b3dfa368f9b11c592a84daa424599f405aba86
redn1njaa <ostap.seryvko@ucu.edu.ua>
2022-06-03 18:02:01 +0300

    states generated

--- cellular_automaton.py
+++ cellular_automaton.py
@@ -1,7 +1,31 @@
+import numpy as np
+import random
+from person import Person
+
 
 class CellularAutomaton:
     '''
     Class that represents a grid of people, who interact together and form a society.
     '''
-    def __init__(self, width, height, ua_percentage, youth_percentage, fill):
-        pass
+
+    def __init__(self, width, height, ua_percentage, youth_percentage, adult_percentage, fill):
+        self._grid = np.array([[Person(self.generate_age(youth_percentage, adult_percentage), (j, i), self.generage_state(
+            ua_percentage)) if random.random() <= fill else None for i in width] for j in height])
+
+    @staticmethod
+    def generate_age(prob_y, prob_a):
+        final_prob = random.random()
+        if final_prob <= prob_y:
+            return 0
+        if prob_y < final_prob < prob_y+prob_a:
+            return 1
+        return 2
+
+    @staticmethod
+    def generage_state(prob):
+        states_ukr = ['active_ua', 'passive_ua']
+        states_negative = ['surzhyk', 'passive_ru', 'active_ru']
+        if random.random() <= prob:
+            return random.choice(states_ukr)
+        return random.choice(states_negative)
+    
--- person.py
+++ person.py
@@ -1,21 +1,16 @@
 
 class Person:
-    '''
-    Class that describes a single person
-
-    Possible states:
-        -active_ua
-        -passive_ua
-        -surzhyk
-        -passive_ru
-        -active_ru
-    '''
-    def __init__(self, age, region, coordinates, ukrlang_parents):
+    def __init__(self, age, coordinates, state):
         self.age = age
-        self.region = region
+        # self.region = region
         self.coordinates = coordinates
-        self.parents = ukrlang_parents
-        self.state = None
+        # self.parents = ukrlang_parents
+        self.state = state
 
     def __str__(self) -> str:
-        return f'Person({self.age}, {self.region}, {self.coordinates}, {self.parents}) is in {self.state} state'
+        return f'Person({self.age}, {self.coordinates}) is {self.lang} speaker'
+    
+    def __repr__(self) -> str:
+        return self.lang
+    
+
