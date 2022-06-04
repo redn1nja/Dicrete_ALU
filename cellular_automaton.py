fe0e0927223338ebb6d1595a581a392ecd359a9f
Teodor Muzychuk <teodor.muzychuk@ucu.edu.ua>
2022-06-04 13:56:49 +0300

    halting problem

--- cellular_automaton.py
+++ cellular_automaton.py
@@ -1,6 +1,8 @@
 import numpy as np
 import random
 from person import Person
+from time import sleep
+from os import system
 
 
 class CellularAutomaton:
@@ -8,7 +10,8 @@ class CellularAutomaton:
     Class that represents a grid of people, who interact together and form a society.
     '''
 
-    def __init__(self, width, height, ua_percentage, youth_percentage, adult_percentage, fill):
+    def __init__(self, width, height, ua_percentage, youth_percentage,
+                 adult_percentage, fill):
         self._grid = np.array([[Person(self.generate_age(youth_percentage, adult_percentage), (j, i), self.generage_state(
             ua_percentage)) if random.random() <= fill else None for i in range(width)] for j in range(height)])
 
@@ -23,8 +26,8 @@ class CellularAutomaton:
 
     @staticmethod
     def generage_state(prob):
-        states_ukr = ['active_ua', 'passive_ua']
-        states_negative = ['surzhyk', 'passive_ru', 'active_ru']
+        states_ukr = [Person.ACTIVE_UA, Person.PASSIVE_UA]
+        states_negative = [Person.SURZHYK, Person.PASSIVE_RU, Person.ACTIVE_RU]
         if random.random() <= prob:
             return random.choice(states_ukr)
         return random.choice(states_negative)
@@ -42,30 +45,47 @@ class CellularAutomaton:
         affect = 0
         for i in range(-1, 2):
             for j in range(-1, 2):
-                if not (i == 0 and j == 0):
+                if not (i == 0 and j == 0) and len(self._grid) > y+i >= 0\
+                        and len(self._grid[y+i]) > x+j >= 0:
                     if self._grid[y + i, x + j] is None:
                         continue
 
-                    if self._grid[y + i, x + j].state == 'active_ua':
+                    if self._grid[y + i, x + j].state == Person.ACTIVE_UA:
                         affect += 0.125
-                    elif self._grid[y + i, x + j].state == 'passive_ua':
+                    elif self._grid[y + i, x + j].state == Person.PASSIVE_UA:
                         affect += 0.0625
-                    elif self._grid[y + i, x + j].state == 'passive_ru':
+                    elif self._grid[y + i, x + j].state == Person.PASSIVE_RU:
                         affect -= 0.0625
-                    elif self._grid[y + i, x + j].state == 'active_ru':
+                    elif self._grid[y + i, x + j].state == Person.ACTIVE_RU:
                         affect -= 0.125
         return affect
 
     def evolve(self):
         '''
-        Iterates over all the people in the system and changes their state according to the neighbours.
+        Iterates over all the people in the system and changes their state
+        according to the neighbours.
         '''
-        pass
+        for row in self._grid:
+            for person in row:
+                if person is not None:
+                    person.transition_prob += \
+                        self.get_neighbours_affect(*person.coordinates)
+
+        # prob = random.random()
+        prob = random.random()
+        for row in self._grid:
+            for person in row:
+                if person is not None:
+                    person.change_state(prob)
 
     def __repr__(self):
         return str(self._grid)
 
 
-a = CellularAutomaton(8, 8, 0.5, 0.5, 0.2, 0.5)
-print(a)
-print(a.get_neighbours_affect(3,5))
+if __name__ == "__main__":
+    a = CellularAutomaton(8, 8, 0.5, 0.5, 0.2, 0.5)
+    for i in range(100):
+        system("clear")
+        print(a)
+        a.evolve()
+        sleep(0.5)
--- person.py
+++ person.py
@@ -1,3 +1,7 @@
+"""
+Person implementation
+"""
+
 
 class Person:
     '''
@@ -15,22 +19,32 @@ class Person:
         - adult (1)
         - senior (2)
     '''
+    ACTIVE_UA = 4
+    PASSIVE_UA = 3
+    SURZHYK = 2
+    PASSIVE_RU = 1
+    ACTIVE_RU = 0
+    TRANSITION_LIST = ["active_ru", "passive_ru", "surzhyk",
+                       "passive_ua", "active_ua"]
 
     def __init__(self, age, coordinates, state):
         self.age = age
-        self.coordinates = coordinates # (y,x) for the position on the grid
+        self.coordinates = coordinates  # (y,x) for the position on the grid
         self.state = state
-        self.transition_prob = 0 # 1 for more possible transition to proukrainian, -1 for transition to prorussian, 
+        # 1 for more possible transition to proukrainian, -1 for transition to prorussian,
+        self.transition_prob = 0
 
     def __str__(self) -> str:
         return f'Person({self.age}, {self.coordinates}) is in {self.state} state'
 
-    def change_state(self):
+    def change_state(self, prob):
         '''
         Updates transition_prob according to the neighbours.
-        Generates random.random and checks if the state can be changed.
         '''
-        pass
-    
+        if prob < abs(self.transition_prob):
+            self.state += int(self.transition_prob / abs(self.transition_prob))
+            self.state = self.ACTIVE_UA if self.state > self.ACTIVE_UA else self.state
+            self.state = self.ACTIVE_RU if self.state < self.ACTIVE_RU else self.state
+
     def __repr__(self) -> str:
-        return self.state
+        return self.TRANSITION_LIST[self.state]
