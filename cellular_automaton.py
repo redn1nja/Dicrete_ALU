4af3dbbae8293df6924f3840353329623312c6c6
Roman <romanmutel@gmail.com>
2022-06-04 17:19:25 +0300

    bare minimum for vizualization

--- cellular_automaton.py
+++ cellular_automaton.py
@@ -111,6 +111,9 @@ class CellularAutomaton:
     def __repr__(self):
         return "\n".join(" ".join(str(item) if item else "empt" for item in row) for row in self._grid)
 
+    def get_grid(self):
+        return self._grid
+
 
 if __name__ == "__main__":
     a = CellularAutomaton(4, 5, 0.5, 0.5, 0.2, 0.7)
